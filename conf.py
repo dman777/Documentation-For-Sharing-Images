5b741a81672ef0a7f9344c5624c05fd980c9baae
dman777 <darin.hensley@gmail.com>
2014-03-10 09:57:13 -0500

    added to do list

--- conf.py
+++ conf.py
@@ -32,6 +32,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
 ]
+[extensions]
+todo_include_todos=True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
